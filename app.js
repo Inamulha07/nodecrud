bd939a7f21cabe0cef221fe5c329e1aa3e083e35
Inam <inamrao06@gmail.com>
2023-01-11 21:48:18 +0500

    run code

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const http = require('http');
-const server =  http.createServer();
-const app = require('./server');
+const { json } = require('express');
+const express =  require('express');
+const app = express();
 
-server.listen(3000,console.log('app is running'));
+
+app.use((req,res,next)=>{
+    console.log("app is running");
+    res.status(200).json({
+        message: "app is running"
+    })
+})
+
+module.exports = app;
